e95b46281d711ad949b74ae6997a7a8b257212e3
Luis Manuel Rodriguez <lwe_@hotmail.com>
2016-12-03 13:48:52 -0600

    Added initial solution

---
+++ Orders/Orders/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Orders.Views;
+using Xamarin.Forms;
+
+namespace Orders
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Products());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
